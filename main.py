38317ec438f1dee5264a8aa0c8d8d596e021ce4c
Cihan Erdogan <cihanerdogan64@gmail.com>
2023-10-24 16:54:05 +0300

    main.py eklendi

---
+++ main.py
@@ -0,0 +1,28 @@
+import logging
+
+from function.helper_functions import *
+from config.conf import *
+import os
+import argparse
+from logger import logger, console_handler
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-j", "--job_title", default="data engineer", help="Enter the job title")
+parser.add_argument("-l", "--location", default="Turkey", help="Enter the location")
+parser.add_argument("-d", "--debug", action="store_true", help="Debug Mode")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    is_debug = args.debug
+    # print(is_debug)
+    if is_debug:
+         console_handler.setLevel(logging.DEBUG)
+         logger.info("DEBUG mode activated.")
+
+    job_title = args.job_title
+    location = args.location.lower()
+    job_details = generate_job_details_csv("data scientist", "SOUTH KOREA", DEBUG=is_debug)
